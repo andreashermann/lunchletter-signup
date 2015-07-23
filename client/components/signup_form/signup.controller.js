3e78a5a532cd7c896dc7214cc8bd37cb35357c12
jan <jan@jan-Aspire-5742G.(none)>
2015-07-23 14:48:57 +0200

    email form + submit

--- client/components/signup_form/signup.controller.js
+++ client/components/signup_form/signup.controller.js
@@ -1,3 +1,24 @@
-/**
- * Created by jan on 7/23/15.
- */
+'use strict';
+
+angular.module('lunchletterSignupApp')
+  .controller('SignupCtrl', function ($scope, $http, $location) {
+    $scope.submit = function() {
+      $scope.user.location = $location;
+
+      var dataObj = {
+        "event" : "$set",
+        "entityType" : "user",
+        "entityId" : $scope.user.email
+      }
+
+      var ACCESS_KEY = 123;
+
+      var request = $http.post("http://localhost:9000?accessKey="+ACCESS_KEY, dataObj)
+        .error(function(data, status, headers, config) {
+          $scope.requestMessage = "error: " + status.toString() +" "+ data;
+        })
+        .success(function(data, status, headers, config) {
+          $scope.requestMessage = "success: " + status.toString() +" "+ data;
+        });
+    }
+  });
