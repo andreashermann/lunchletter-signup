a0cfe7951167ea531d3bf1f143f62acdbd28105e
Roman Studer <roman.studer@ergon.ch>
2015-07-24 11:24:16 +0200

    add feedback interface

--- server/routes.js
+++ server/routes.js
@@ -11,6 +11,7 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
+  app.use('/feedback', require('./feedback'));
   
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
