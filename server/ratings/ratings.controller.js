f6d72afaffcaacd50071fdf3e1fd62452f4797f4
Andreas Hermann <a.v.hermann@gmail.com>
2015-08-09 00:36:01 +0200

    Fix problem in ratings rest service

--- server/ratings/ratings.controller.js
+++ server/ratings/ratings.controller.js
@@ -15,22 +15,23 @@ var accessKey = process.env.ENGINE_ACCESS_KEY;
 // Get list of ratings
 exports.index = function(req, res) {
   var url = "http://lunchletter.ch:7070/events.json?accessKey=" + accessKey + "&event=rate&limit=1000";
-  
-  if (req.query.entity) {
+
+  if (req.query.entityId) {
   	url += "&entityId=" + req.query.entityId;
   }
   
+  console.log("get ratings", url);
   var response = request(
-  	{ 
-  		url: url, 
+  	{
+  		url: url,
 		method: "GET",
 		json: true,
 		headers: {
         	"content-type": "application/json",
 		},
 	},
-    function(err, res2, ratings) {
-		res.json(ratings);
+  	function(err, res2, ratings) {
+		res.status(200).json(ratings);
 	}
   );
 };
@@ -40,13 +41,13 @@ exports.destroy = function(req, res) {
   var eventId = req.params.id;
   console.log("delete rating " + eventId);
   var url = "http://lunchletter.ch:7070/events/"+eventId+".json?accessKey=" + accessKey;
-  var response = request({ url: url, 
+  var response = request({ url: url,
 	method: "DELETE",
 	json: true,
 	headers: {
 			"content-type": "application/json",
 	},
-  }, 
+  },
   function(err, res2, body) {
 	if(_.isEmpty(err)){
 		res.status(200).send();
