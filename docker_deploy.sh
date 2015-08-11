ae9dc5897a76b6d5a847c34d68e14191eef4fda7
Andreas Hermann <a.v.hermann@gmail.com>
2015-08-11 23:13:43 +0200

    Add keen.io API Key
    Required for event logging

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -2,4 +2,10 @@
 set -x
 docker stop signup-prod 
 docker rm signup-prod 
-docker run -d --name signup-prod -e NODE_ENV=production -e ENGINE_ACCESS_KEY=$ENGINE_ACCESS_KEY -e IP=0.0.0.0 -p 80:8080 lunchletter/signup
+docker run -d --name signup-prod \
+	-e NODE_ENV=production \
+	-e ENGINE_ACCESS_KEY=$ENGINE_ACCESS_KEY \
+	-e KEEN_WRITE_KEY=$KEEN_WRITE_KEY \
+	-e IP=0.0.0.0 \
+	-p 80:8080 \
+	lunchletter/signup
